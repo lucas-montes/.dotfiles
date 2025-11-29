5ef26ce41d8413d77b484285cc5b7a6afa5abf90
Lucas <lluc23@hotmail.com>
2025-11-29 09:34:26 +0100

    add amd machine

---
+++ hosts/luctop-amd/configuration.nix
@@ -0,0 +1,55 @@
+{
+  pkgs,
+  stateVersion,
+  hostname,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    ../../settings
+    ../../services/postgres.nix
+  ];
+
+  environment.systemPackages = [
+    pkgs.home-manager
+    pkgs.curl
+    pkgs.wget
+    pkgs.git
+    pkgs.cudatoolkit
+  ];
+
+  networking.hostName = hostname;
+  hardware.tuxedo-control-center.enable = true;
+
+  services = {
+    printing.enable = true;
+    xserver.xkb = {
+      layout = "us";
+      variant = "";
+    };
+    gnome.gnome-keyring = {enable = true;};
+  };
+
+  programs = {
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+    nix-ld.enable = true;
+    seahorse.enable = true;
+  };
+
+  virtualisation.docker.enable = true;
+  # Files, browser, screen sharing stuff
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
+
+  system.stateVersion = stateVersion;
+}
