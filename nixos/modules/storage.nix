05a8c1562218d911c114e475955055b7ccd15127
Lucas <lluc23@hotmail.com>
2025-11-30 08:43:33 +0100

    this avoid hanging on the graphique interface when booting

--- nixos/modules/storage.nix
+++ nixos/modules/storage.nix
@@ -2,9 +2,9 @@
   services.gvfs.enable = true; # For nautilus to use trash
   services.tumbler.enable = true; # For preview images
   services.udisks2.enable = true;
-  fileSystems."/data" = {
-    device = "/dev/disk/by-label/SSD-1TB";
-    fsType = "vfat";
-    options = ["defaults" "user" "rw" "utf8" "umask=000"];
-  };
+  # fileSystems."/data" = {
+  #   device = "/dev/disk/by-label/SSD-1TB";
+  #   fsType = "vfat";
+  #   options = ["defaults" "user" "rw" "utf8" "umask=000"];
+  # };
 }
