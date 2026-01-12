c5db0ff77cfe8101976c78865ae73cff58257d8e
Lucas <lluc23@hotmail.com>
2026-01-12 19:36:24 +0100

    add creation of some dirs ant startup

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,21 +1,23 @@
-{
-  stateVersion,
-  mainUser,
-  ...
-}: {
-  imports = [
-    ./modules
-    ./home-packages.nix
-  ];
+{ lib, stateVersion, mainUser, ... }: {
+  imports = [ ./modules ./home-packages.nix ];
 
   home = {
     inherit stateVersion;
     username = mainUser;
     homeDirectory = "/home/${mainUser}";
+    activation = {
+      myActivationAction = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        run mkdir -p ~/Downloads/music
+        run mkdir -p ~/Notes
+        run mkdir -p ~/Projects
+        if [ ! -d ~/Projects/procurator ]; then
+          run git clone git@github.com:lucas-montes/procurator.git ~/Projects/procurator
+        fi
+        verboseEcho "Activation complete for user directories."
+      '';
+    };
   };
 
-  home.sessionVariables = {
-    XDG_SESSION_TYPE="wayland";
-  };
+  home.sessionVariables = { XDG_SESSION_TYPE = "wayland"; };
 
 }
