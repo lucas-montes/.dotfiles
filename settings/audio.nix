f305b49f2e18aa51d6bbc90652e5f2a702809a1a
Lucas <lluc23@hotmail.com>
2026-02-09 12:38:35 +0100

    fix latest amd cpus audio issues
    Signed-off-by: Lucas <lluc23@hotmail.com>

--- settings/audio.nix
+++ settings/audio.nix
@@ -6,5 +6,11 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
+    # Audio issue with latest amd https://github.com/NixOS/nixos-hardware/issues/1603
+    wireplumber.extraConfig.no-ucm = {
+      "monitor.alsa.properties" = {
+        "alsa.use-ucm" = false;
+      };
+    };
   };
 }
