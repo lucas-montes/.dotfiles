291b4e5f26feec682d594033ccdab06d7079b051
Lucas <lluc23@hotmail.com>
2026-04-26 15:17:20 +0200

    fix bridge address in the vmm nixos module
    Signed-off-by: Lucas <lluc23@hotmail.com>

--- services/procurator.nix
+++ services/procurator.nix
@@ -3,8 +3,7 @@
     # Enable and configure the VMM host networking and worker service
     vmm = {
       enable = true;
-      externalInterface = "wlp98s0"; # set to your host uplink
-      bridgeAddress = "192.168.100.1";
+      externalInterface = "wlp98s0";
     };
     worker = {
       enable = false;
