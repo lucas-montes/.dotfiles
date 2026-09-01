5c4f075f6b6ef5e1ce201355c02f306400b4854d
Lucas <lluc23@hotmail.com>
2026-09-01 18:01:13 +0200

    add some filter for dns that tries to get ads
    Signed-off-by: Lucas <lluc23@hotmail.com>

--- settings/net.nix
+++ settings/net.nix
@@ -1,8 +1,25 @@
-{ lib, ... }: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   networking.networkmanager.enable = true;
-  networking.networkmanager.dns = "systemd-resolved";
+  networking.networkmanager.dns = "none";
   networking.networkmanager.unmanaged = ["interface-name:br0"];
   networking.useDHCP = lib.mkForce false;
   networking.dhcpcd.enable = lib.mkForce false;
-  services.resolved.enable = true;
+  services.resolved.enable = lib.mkForce false;
+
+  services.dnsmasq = {
+    enable = true;
+    resolveLocalQueries = lib.mkForce false;
+    settings = {
+      cache-size = 10000;
+      addn-hosts = "${pkgs.stevenblack-blocklist}/hosts";
+      domain-needed = true;
+      bogus-priv = true;
+    };
+  };
+
+  networking.nameservers = lib.mkForce ["127.0.0.1"];
 }
